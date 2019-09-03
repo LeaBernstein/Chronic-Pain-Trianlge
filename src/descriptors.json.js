373f40c852ec138f55933d56509595eb6a138b9b
LeaBernstein <54841331+LeaBernstein@users.noreply.github.com>
2019-09-03 11:48:35 +0100

    Initial commit

---
+++ src/descriptors.json.js
@@ -0,0 +1,159 @@
+export default {
+  Relationships: {
+    __description: "",
+    Self: {
+      "Feeling disintegrated/alienated": null,
+      "Self-worth": null,
+      "Lack of confidence": null
+    },
+    Partner: {
+      "Intimacy/Sex": null,
+      "Feeling disintegrated": null,
+      Communication: null,
+      Dependence: null
+    },
+    Family: {
+      "Inability to fulfil responsibilities": null,
+      Dependence: null,
+      "Social Fatigue": null,
+      "Role conflict": null
+    }
+  },
+  Fatigue: {
+    __description: " ",
+    Physically: {
+      __description: " ",
+      "Everyday activities": null,
+      "Strenuous exercise": null,
+      Walking: null
+    },
+    Mentally: {
+      __description: " ",
+      Forgetfulness: null,
+      "Poor concentration": null,
+      "Difficulties completing tasks": null
+    },
+    Emotionally: {
+      __description: " ",
+      Self: null,
+      "Feeling overburdened": null,
+      Irritability: null,
+      Frustration: null
+    }
+  },
+  Sleep: {
+    __description: " ",
+    "Disturbed sleep": {
+      "Trouble falling asleep": null,
+      "Trouble waking up": null,
+      "Trouble remaining asleep": null
+    },
+    "Physical troubles": {
+      __description: " ",
+      "Sleep-disordered breathing": null,
+      "Sleep-related headaches": null,
+      "Bad sleeping position": null,
+      "Napping during the day": null,
+      Stiffness: null
+    },
+    "Sleep medication": {
+      __description: " ",
+      "Need for sedatives": null,
+      "Side effects": null,
+      "Daytime sleepiness": null,
+      "effect on other medication": null
+    }
+  },
+  "Emotions/Mood": {
+    __description: " ",
+    Anxiety: {
+      "Feeling on edge": null,
+      Restlessness: null,
+      Irritability: null,
+      Uncertainty: null
+    },
+    Sadness: {
+      Loneliness: null,
+      Emptiness: null
+    },
+    Frustration: {
+      Regret: null,
+      "Feeling Of No Control": null,
+      "Lack of Independence": null
+    }
+  },
+  "Physical Health": {
+    __description: " ",
+    Movement: {
+      __description: " ",
+      "Decreased Range Of Motion": null,
+      "Muscle Tension": null,
+      "Light exercice": null,
+      "Strenuous exercise": null
+    },
+
+    Comorbidities: {
+      __description: " ",
+      "Sleep problems": null,
+      "Depressed mood": null,
+      Anxiety: null,
+      "General health/wellbeing": null
+    },
+    Medication: {
+      __description: " ",
+      "Withdrawal symptpms": null,
+      "Financial pressure": null,
+      "Insufficient Pain Relief": null
+    }
+  },
+  Work: {
+    __description: " ",
+    Abiliy: {},
+    Desire: {
+      Financial: null,
+      Purpose: null,
+      "Extended absense": null,
+      "stigma of unemployment": null
+    },
+    Need: {}
+  }
+};
+
+/*
+		this.descriptors = [
+			{
+				name: 'Appearance',
+				children: [
+					{name: 'Self',           children: []},
+					{name: 'Others Private', children: []},
+					{name: 'Others Public',  children: []},
+				]
+			},
+			{
+				name: 'Eating/Drinking',
+				children: [
+					{name: 'Inability to Swallow', children: [
+						{name: 'Dry Mouth', children: []},
+						{name: 'Fluids',    children: []},
+						{name: 'Solids',    children: []},
+					]},
+					{name: 'Loss of Appetite',     children: []},
+					{name: 'Impaired Taste/Smell', children: []},
+					{name: 'Inability to Chew',    children: []},
+					{name: 'Embarassment',         children: []},
+					{name: 'Voluntary/involuntary swallowing', children: []},
+				]
+			},
+			{
+				name: 'Fatigue',
+				children: [
+					{name: 'Physically Exhausted', children: [
+						{name: 'Unable to do everyday stuff', children: []},
+						{name: 'Unable to do gentle exercise',    children: []},
+						{name: 'Unable to do strenuous exercise',    children: []},
+					]},
+					{name: 'Mentally Exhausted',     children: []},
+					{name: 'Emotionally Exhausted', children: []},
+				]
+			},
+			];*/
---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import reducer from "./reducers/index.js";
+import { addResult } from "./reducers/results.js";
+import { finishQuiz } from "./reducers/gui.js";
+import Set from "./components/set.js";
+import Table from "./components/table.js";
+
+require("./style.scss");
+
+let store = createStore(reducer);
+
+const getRootResults = results => results.filter(r => !r.has("parent"));
+
+const mapStateToProps = ({ results, gui }) => {
+  return {
+    results: getRootResults(results),
+    finished: gui.get("finished")
+  };
+};
+
+const mapDispatchToProps = {
+  onClick: addResult,
+  onFinish: finishQuiz
+};
+
+const MagicTriangle = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(({ results, finished, onClick, onFinish }) => (
+  <div>
+    {!finished && (
+      <div>
+        <div className="magic-triangle">
+          {results.valueSeq().map((r, idx) => (
+            <Set
+              giveInstructions={idx === 0}
+              key={r.get("id")}
+              root={r.get("id")}
+            />
+          ))}
+        </div>
+        <p>If you're done you can:</p>
+        <p>
+          <button onClick={onClick}>Tell us about another problem</button> or{" "}
+          <button onClick={onFinish}>Finish</button>
+        </p>
+      </div>
+    )}
+    {finished && (
+      <Table
+        results={results.filter(r => typeof r.get("ratios") !== "undefined")}
+      />
+    )}
+  </div>
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <MagicTriangle />
+  </Provider>,
+  document.getElementById("root")
+);
